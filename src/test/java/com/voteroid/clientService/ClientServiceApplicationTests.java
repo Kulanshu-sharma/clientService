f1223f036d5a46751e4bb3d56cbf4ff122e3921e
Kulanshu Sharma <kulanshusharma@192.168.1.13>
2022-08-10 20:20:13 +0530

    Creating Client Project

---
+++ src/test/java/com/voteroid/clientService/ClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voteroid.clientService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
